b6221d45a1f9b663eef73f196e6158f64e3de202
Hongmin Liu <hongmin.liu@cyara.com>
2019-12-08 23:16:00 +1100

    spectest
    Added sample specflow test

---
+++ DeveloperTestCyara/DeveloperTest/Palindrome.Specs/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Palindrome.Specs
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
