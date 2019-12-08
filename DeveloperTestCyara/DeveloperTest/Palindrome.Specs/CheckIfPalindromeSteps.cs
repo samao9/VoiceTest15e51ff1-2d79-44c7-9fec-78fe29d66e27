8c351700c07e0d861ae4b8ab5e7b8fcb9cee4c1c
Hongmin Liu <hongmin.liu@cyara.com>
2019-12-09 00:00:17 +1100

    added tests
    Added specflow tests for Ispalindrome

---
+++ DeveloperTestCyara/DeveloperTest/Palindrome.Specs/CheckIfPalindromeSteps.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Palindrome.Specs
+{
+    [Binding]
+    public class CheckIfPalindromeSteps
+    {
+        private bool result;
+        [Given(@"I have entered ""([a-z]*)"" as the input")]
+        public void GivenIHaveEnteredAsTheInput(string word)
+        {
+            result = PalindromeTransform.IsPalindrome(word);
+        }
+        
+        [Then(@"I got a true as result")]
+        public void ThenIGotATrueAsResult()
+        {
+            Assert.AreEqual(true, result);
+        }
+
+        [Then(@"I got a false as result")]
+        public void ThenIGotAFalseAsResult()
+        {
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ DeveloperTestCyara/DeveloperTest/Palindrome.Specs/IfPalindrome.feature
@@ -0,0 +1,25 @@
+﻿Feature: Check if palindrome
+	In order to verify if a string is palindrome
+	As a tester
+	I want to be told true/false if a string [a-z] is/not a palindrome
+
+@palindromeTests
+Scenario: If a palindrome
+	Given I have entered "<word>" as the input
+	Then I got a true as result
+
+	Examples: 
+	| word   |
+	| a       |
+	| aa      |
+	| racecar |
+
+Scenario: If not a palindrome
+	Given I have entered "<word>" as the input
+	Then I got a false as result
+
+	Examples: 
+	| word   |
+	| baba       |
+	| ab      |
+	| raceciar |
